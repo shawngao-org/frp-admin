fd9353407929f82425f8889cb0203bd9526f04ed
ShawnGao <2519306859@qq.com>
2024-04-18 19:54:02 +0800

    :bug: Fix wrong project name.

--- build.bat
+++ build.bat
@@ -4,4 +4,4 @@ set /p targetPlatform="Enter target platform[386, amd64, arm]: "
 SET  CGO_ENABLED=0
 SET GOOS=%targetSystem%
 SET GOARCH=%targetPlatform%
-go build cloud-lock-go-gin
+go build frp-admin
