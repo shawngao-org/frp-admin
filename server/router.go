d5120dc875550d5467e8cfb8be3a4df9454bc206
ShawnGao <2519306859@qq.com>
2024-04-19 20:28:51 +0800

    :sparkles: API document.

--- server/router.go
+++ server/router.go
@@ -1,18 +1,32 @@
 package server
 
 import (
+	"frp-admin/api"
+	"frp-admin/config"
+	"frp-admin/docs"
 	"github.com/gin-gonic/gin"
-	"net/http"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+const apiPrefix = "/api/v1"
 
 func LoadRouter(r *gin.Engine) {
 	RootRouter(r)
 }
 
 func RootRouter(r *gin.Engine) {
-	r.GET("/ping", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"message": "pong!",
-		})
-	})
+	v1 := r.Group(apiPrefix)
+	{
+		v1.GET("/ping", api.Ping)
+		v1.POST("/login", api.Login)
+
+		docs.SwaggerInfo.Title = "API Docs"
+		docs.SwaggerInfo.Description = "null."
+		docs.SwaggerInfo.Version = "1.0"
+		docs.SwaggerInfo.Host = config.Conf.Server.Ip + ":" + config.Conf.Server.Port
+		docs.SwaggerInfo.BasePath = "/"
+		docs.SwaggerInfo.Schemes = []string{"http", "https"}
+		v1.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	}
 }
